0a74e6b0fbfad6b784d09a1a8622db3eb24f0d24
Thiago Carneiro <thijack2000@gmail.com>
2022-05-24 11:42:35 -0300

    inclusao discovery bitbucket e alteracao para tech docs

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,12 +2,14 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { BitbucketDiscoveryProcessor } from '@backstage/plugin-catalog-backend-module-bitbucket';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addProcessor(BitbucketDiscoveryProcessor.fromConfig(env.config, { logger: env.logger }));
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
